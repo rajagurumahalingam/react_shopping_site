9801fe127057ace1159e3ab1e4ee628577a01d75
Rajaguru <mrguru1115@gmail.com>
2021-10-10 18:08:20 +0530

    React Router added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App"></div>
+    <Router>
+      <div className="App">
+        <h1>Amazon Clone!</h1>
+        <Switch>
+          <Route path="/">
+            <h1>Home page!</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login page!</h1>
+          </Route>
+          <Route path="/checkout">
+            <h1>Check out page!</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
